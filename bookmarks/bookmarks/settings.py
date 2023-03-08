a1ba36c025fe36c9e096e64dcf0d94e9fe341208
cescalante-dev <carlos.dev.escalante@gmail.com>
2023-03-07 22:03:50 -0600

    Add auth templates

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -15,6 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
